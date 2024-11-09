e7c629ec5bbd8a2677b02e3f74e86122ad8991ae
Dane <112781431+Githubuser1911@users.noreply.github.com>
2024-11-09 15:11:09 -0800

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+
+window.onscroll = function() {
+  const backToTopButton = document.getElementById("backToTop");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      backToTopButton.style.display = "block";
+  } else {
+      backToTopButton.style.display = "none";
+  }
+};
+
+// Smooth scroll back to top
+document.getElementById("backToTop").addEventListener("click", function(event) {
+  event.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
